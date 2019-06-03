bfac7c98dcad16eaf41a574e68be0e2cd668c087
Spshake <amx_cfg@hotmail.com>
2019-06-03 11:12:51 -0300

    Tributos agregados

--- src/org/persona/Persona.java
+++ src/org/persona/Persona.java
@@ -13,15 +13,24 @@ public class Persona {
     
     private String nombreApellido; 
     private String correo;
+    private String dni;
+    private String provincia;
+    private String ciudad;
+    private String domicilio;
+    private String fechaNac;
+    private String telefono;
 
-    
-    //Constructor
-    public Persona(String nombreApellido, String correo) {
+    public Persona(String nombreApellido, String correo, String dni, String provincia, String ciudad, String domicilio, String fechaNac, String telefono) {
         this.nombreApellido = nombreApellido;
         this.correo = correo;
+        this.dni = dni;
+        this.provincia = provincia;
+        this.ciudad = ciudad;
+        this.domicilio = domicilio;
+        this.fechaNac = fechaNac;
+        this.telefono = telefono;
     }
-    
-    //Get and Set
+
     public String getNombreApellido() {
         return nombreApellido;
     }
@@ -37,5 +46,54 @@ public class Persona {
     public void setCorreo(String correo) {
         this.correo = correo;
     }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(String fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
     
 }
